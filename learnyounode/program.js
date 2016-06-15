6fce9aed408cdb5ad7a45915e9b2d71b0babac87
Danny Harlav <danh@lolaent.com>
2016-06-15 15:12:10 +0300

    HTTP fix

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -1,64 +1,61 @@
 /*
 
- HTTP CLIENT
- Exercise 7 of 13
+   JUGGLING ASYNC
+   Exercise 9 of 13
 
-Write a program that performs an HTTP GET request to a URL provided to you as
-the first command-line argument. Write the String contents of each "data" event
-from the response to a new line on the console (stdout).
+  This problem is the same as the previous problem (HTTP COLLECT) in that you
+  need to use http.get(). However, this time you will be provided with three
+  URLs as the first three command-line arguments.
 
--------------------------------------------------------------------------------
+  You must collect the complete content provided to you by each of the URLs and
+  print it to the console (stdout). You don't need to print out the length, just
+  the data as a String; one line per URL. The catch is that you must print them
+  out in the same order as the URLs are provided to you as command-line
+  arguments.
 
-## HINTS
+  -------------------------------------------------------------------------------
 
-For this exercise you will need to use the http core module.
+  ## HINTS
 
-Documentation on the http module can be found by pointing your browser here:
-  file:///usr/local/share/npm/lib/node_modules/learnyounode/node_apidoc/http.html
+  Don't expect these three servers to play nicely! They are not going to give
+  you complete responses in the order you hope, so you can't naively just print
+  the output as you get it because they will be out of order.
 
-The http.get() method is a shortcut for simple GET requests, use it to simplify
-your solution. The first argument to http.get() can be the URL you want to GET;
-provide a callback as the second argument.
+  You will need to queue the results and keep track of how many of the URLs have
+  returned their entire contents. Only once you have them all, you can print the
+  data to the console.
 
-Unlike other callback functions, this one has the signature:
+  Counting callbacks is one of the fundamental ways of managing async in Node.
+  Rather than doing it yourself, you may find it more convenient to rely on a
+  third-party library such as [async](http://npm.im/async) or [after](http://npm.im/after).
+  But for this exercise, try and do it without any external helper library.
 
-    function callback (response) { /* ... * }
-
-Where the response object is a Node Stream object. You can treat Node Streams as
-objects that emit events. The three events that are of most interest are: "data"
-, "error" and "end". You listen to an event like so:
-
-    response.on("data", function (data) {  ...  })
-
-The "data" event is emitted when a chunk of data is available and can be
-processed. The size of the chunk depends upon the underlying data source.
-
-The response object / Stream that you get from http.get() also has a
-setEncoding() method. If you call this method with "utf8", the "data" events
-will emit Strings rather than the standard Node Buffer objects which you have to
-explicitly convert to Strings.
-
--------------------------------------------------------------------------------
-
- » To print these instructions again, run: learnyounode print
- » To execute your program in a test environment, run: learnyounode run program.js
- » To verify your program, run: learnyounode verify program.js
- » For help run: learnyounode help
+  -------------------------------------------------------------------------------
 
 */
 
-var mymodule = require('./node-module');
+var http = require('http');
 
-var myCallback = function(err, result) {
-    if (err) {
-        console.log(err);
-    } else {
-        result.forEach(function(item) {
-            console.log(item);
+var getContent = function(urls) {
+    var url = urls.shift();
+    http.get(url, function(response) {
+        var fullData = '';
+        response.setEncoding('utf8');
+
+        response.on('data', function(data) {
+            fullData = fullData + data;
+        }).on('end', function() {
+            console.log(fullData);
+            if (urls.length) {
+              getContent(urls);
+            }
         });
-    }
-};
+    });
+}
 
-var dirPath = process.argv[2];
-var extensionFilter = process.argv[3];
-mymodule(dirPath, extensionFilter, myCallback);
+urls = [];
+for (var i = 2; i <= 4; i++) {
+  urls.push(process.argv[i]);
+}
+
+getContent(urls);
